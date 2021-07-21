12a2ee0b8ff91d92467cabfe3db9cd7a4d92f379
Dor-Karni <Dor.Karni@exlibrisgroup.com>
2021-07-21 09:35:11 +0300

    Small updates

--- cloudapp/src/app/service/catalog.service.ts
+++ cloudapp/src/app/service/catalog.service.ts
@@ -13,12 +13,6 @@ import { Name } from '../catalog/results-types/name';
 import { UniformTitle } from '../catalog/results-types/uniformTitle';
 
 
-import  *  as  monographsDataFile  from  'C:/Users/dork/Documents/URMs/Nacsis/copy catalog frontend/monography.json'
-import  *  as  namesDataFile  from  'C:/Users/dork/Documents/URMs/Nacsis/copy catalog frontend/name.json';
-import  *  as  serialsDataFile  from  'C:/Users/dork/Documents/URMs/Nacsis/copy catalog frontend/serial.json';
-import  *  as  uniformTitlesDataFile  from  'C:/Users/dork/Documents/URMs/Nacsis/copy catalog frontend/uniformTitle.json';
-
-
 
 @Injectable({
     providedIn: 'root'
@@ -29,7 +23,6 @@ export class CatalogService extends BaseService {
     private searchResultsMap: Map<SearchType, NacsisCatalogResults>;
 
     public queryParams: string = "";
-    // public queryParams: string = "fromIndex=1&toIndex=20&searchType=Monographs&database=BOOK,PREBOOK,JPMARC&ID=12345";
 
     constructor(
         protected eventsService: CloudAppEventsService,
@@ -60,10 +53,6 @@ export class CatalogService extends BaseService {
         return this.queryParams;
     }
 
-    // setQueryParams(queryParams: string) {
-    //     this.queryParams = queryParams;
-    // }
-
     setBaseUrl(initData: InitData) : string {
         let baseUrl = super.setBaseUrl(initData);
         baseUrl = baseUrl + "copyCatalog?";
@@ -85,7 +74,6 @@ export class CatalogService extends BaseService {
                 return this.http.get<any>(fullUrl, { headers })
             }),
             mergeMap(response => {
-                // this.setResultsDataFromResponse(searchType);
                 if (response.status === this.OkStatus) {
                     this.searchResultsMap.get(searchType).setHeader(response);
                     this.searchResultsMap.get(searchType).setResults(new Array());
@@ -119,25 +107,6 @@ export class CatalogService extends BaseService {
         let fromIndex = 1;
         let toIndex = 20;
         return "fromIndex=" + fromIndex + "&toIndex=" + toIndex + "&";
-    }
-    
-    setResultsDataFromResponse(searchType: SearchType){
-        let jsonFile: any;
-        if(searchType == SearchType.Monographs)
-            jsonFile = (monographsDataFile  as  any).default;
-        else if(searchType == SearchType.Serials)
-            jsonFile = (serialsDataFile  as  any).default;
-        else if(searchType == SearchType.Names)
-            jsonFile = (namesDataFile  as  any).default;
-        else if(searchType == SearchType.UniformTitles)
-            jsonFile = (uniformTitlesDataFile  as  any).default;
-           
-        let type = jsonFile.searchType;
-        this.searchResultsMap.get(type).setHeader(jsonFile);
-        this.searchResultsMap.get(type).setResults(new Array());
-        jsonFile.records.forEach(record => {
-            this.searchResultsMap.get(type).getResults().push(this.resultsTypeFactory(type, record));
-        });   
     }
 
 }
