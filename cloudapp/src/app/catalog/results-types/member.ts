9488f7622802d0400d64ed71ee902b39896406fd
dongchch <chenchen.dong@exlibrisgroup.com>
2021-12-14 10:39:42 +0800

    Add translation
    Add translation for member info

--- cloudapp/src/app/catalog/results-types/member.ts
+++ cloudapp/src/app/catalog/results-types/member.ts
@@ -55,7 +55,7 @@ export class MemberFullDisplay extends IDisplayLines {
     initContentDisplay() {
         this.viewLines = new Array<ViewLine>();
         let fieldsArray = new Array<ViewField>();
-        fieldsArray.push(new ViewFieldBuilder().label("Member ID:").content(this.record.ID).build());
+        fieldsArray.push(new ViewFieldBuilder().label("ILL.MemberInfo.MemberID").content(this.record.ID).build());
         fieldsArray.push(new ViewFieldBuilder().label("Create date:").content(this.dateFormatDisplay(this.record.CRTDT)).build());
         fieldsArray.push(new ViewFieldBuilder().label("Update date:").content(this.dateFormatDisplay(this.record.RNWDT)).build());
         this.addLine(new ViewFieldBuilder().build(), fieldsArray);
@@ -63,93 +63,97 @@ export class MemberFullDisplay extends IDisplayLines {
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.NAME).build());
         fieldsArray.push(new ViewFieldBuilder().label("|| ").content(this.record.NAMER).build());
-        this.addLine(new ViewFieldBuilder().label("Participating organization name").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.ParticipatingOrganizationName").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.RYAKU).build());
         fieldsArray.push(new ViewFieldBuilder().label("|| ").content(this.record.RYAKUR).build());
-        this.addLine(new ViewFieldBuilder().label("Participating organization abbreviation").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.ParticipatingOrganizationAbbreviation").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
-        fieldsArray.push(new ViewFieldBuilder().label("Postal Code:").content(this.record.ZIP).build());
-        fieldsArray.push(new ViewFieldBuilder().label("Street Address:").content(this.record.ADDRESS).build());
-        this.addLine(new ViewFieldBuilder().label("Address").build(), fieldsArray);
+        fieldsArray.push(new ViewFieldBuilder().label("ILL.MemberInfo.PostalCode").content(this.record.ZIP).build());
+        fieldsArray.push(new ViewFieldBuilder().label("ILL.MemberInfo.StreetAddress").content(this.record.ADDRESS).build());
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.Address").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.ILLDEPT).build());
-        this.addLine(new ViewFieldBuilder().label("ILL department").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.ILLDepartment").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.ILLSTAFF).build());
-        this.addLine(new ViewFieldBuilder().label("ILL person in charge").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.ILLPersonInCharge").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.TEL).build());
-        this.addLine(new ViewFieldBuilder().label("Phone number").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.PhoneNumber").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.FAX).build());
-        this.addLine(new ViewFieldBuilder().label("Fax").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.Fax").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.SETCODE).build());
-        this.addLine(new ViewFieldBuilder().label("EstablisherType").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.EstablisherType").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.ORGCODE).build());
-        this.addLine(new ViewFieldBuilder().label("Institution type").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.InstitutionType").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.CATFLG).build());
-        this.addLine(new ViewFieldBuilder().label("CAT participation type").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.CATParticipationType").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.ILLFLG).build());
-        this.addLine(new ViewFieldBuilder().label("ILL participation type").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.ILLParticipationType").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.COPYS).build());
-        this.addLine(new ViewFieldBuilder().label("Copy service type").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.CopyServiceType").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.LOANS).build());
-        this.addLine(new ViewFieldBuilder().label("Lending service type").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.LendingServiceType").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.FAXS).build());
-        this.addLine(new ViewFieldBuilder().label("FAX service type").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.FAXServiceType").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.STAT).build());
-        this.addLine(new ViewFieldBuilder().label("Service status").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.ServiceStatus").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.COPYAL).build());
-        this.addLine(new ViewFieldBuilder().label("Copy reception hall code").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.CopyReceptionHallCode").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.LOANAL).build());
-        this.addLine(new ViewFieldBuilder().label("Rental and borrowing center code").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.RentalAndBorrowingCenterCode").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.LOANP).build());
-        this.addLine(new ViewFieldBuilder().label("Lending period").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.LendingPeriod").build(), fieldsArray);
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.KENCODE).build());
-        this.addLine(new ViewFieldBuilder().label("Region (prefecture) code").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.RegionPrefectureCode").build(), fieldsArray);
 
         if (!this.isEmpty(this.record.CATDEPT)) {
             for (let i = 0; i < this.record.CATDEPT.length; i++) {
                 fieldsArray = new Array<ViewField>();
                 fieldsArray.push(new ViewFieldBuilder().content(this.record.CATDEPT[i]).build());
                 if (!this.isEmpty(this.record.CATTEL) && this.record.CATTEL.length >= i) {
-                    fieldsArray.push(new ViewFieldBuilder().label("Phone num.:").content(this.record.CATTEL[i]).build());
+                    if(i==1){
+                        fieldsArray.push(new ViewFieldBuilder().label("ILL.MemberInfo.CatPhoneNumFirst").content(this.record.CATTEL[i]).build());
+                    }else{
+                        fieldsArray.push(new ViewFieldBuilder().label("ILL.MemberInfo.CatPhoneNum").content(this.record.CATTEL[i]).build());
+                    }
                 }
                 if (!this.isEmpty(this.record.CATFAX) && this.record.CATFAX.length >= i) {
-                    fieldsArray.push(new ViewFieldBuilder().label("Fax").content(this.record.CATFAX[i]).build());
+                    fieldsArray.push(new ViewFieldBuilder().label("ILL.MemberInfo.CatFax").content(this.record.CATFAX[i]).build());
                 }
-                this.addLine(new ViewFieldBuilder().label("Region (prefecture) code").build(), fieldsArray);
+                this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.CatalogingDepartment").build(), fieldsArray);
             }
         }
 
@@ -158,12 +162,12 @@ export class MemberFullDisplay extends IDisplayLines {
                 fieldsArray = new Array<ViewField>();
                 fieldsArray.push(new ViewFieldBuilder().content(this.record.SYSDEPT[i]).build());
                 if (!this.isEmpty(this.record.SYSTEL) && this.record.SYSTEL.length >= i) {
-                    fieldsArray.push(new ViewFieldBuilder().label("Phone num.:").content(this.record.SYSTEL[i]).build());
+                    fieldsArray.push(new ViewFieldBuilder().label("ILL.MemberInfo.SystemPhoneNum").content(this.record.SYSTEL[i]).build());
                 }
                 if (!this.isEmpty(this.record.SYSFAX) && this.record.SYSFAX.length >= i) {
-                    fieldsArray.push(new ViewFieldBuilder().label("Fax").content(this.record.SYSFAX[i]).build());
+                    fieldsArray.push(new ViewFieldBuilder().label("ILL.MemberInfo.SystemFax").content(this.record.SYSFAX[i]).build());
                 }
-                this.addLine(new ViewFieldBuilder().label("System department").build(), fieldsArray);
+                this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.SystemDepartment").build(), fieldsArray);
             }
         }
 
@@ -171,7 +175,7 @@ export class MemberFullDisplay extends IDisplayLines {
             for (let i = 0; i < this.record.EMAIL.length; i++) {
                 fieldsArray = new Array<ViewField>();
                 fieldsArray.push(new ViewFieldBuilder().content(this.record.EMAIL[i]).build());
-                this.addLine(new ViewFieldBuilder().label("Email").build(), fieldsArray);
+                this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.Email").build(), fieldsArray);
             }
         }
 
@@ -179,7 +183,7 @@ export class MemberFullDisplay extends IDisplayLines {
             for (let i = 0; i < this.record.POLICY.length; i++) {
                 fieldsArray = new Array<ViewField>();
                 fieldsArray.push(new ViewFieldBuilder().content(this.record.POLICY[i]).build());
-                this.addLine(new ViewFieldBuilder().label("ILLPolicy").build(), fieldsArray);
+                this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.ILLPolicy").build(), fieldsArray);
             }
         }
 
@@ -187,13 +191,13 @@ export class MemberFullDisplay extends IDisplayLines {
             for (let i = 0; i < this.record.LOC.length; i++) {
                 fieldsArray = new Array<ViewField>();
                 fieldsArray.push(new ViewFieldBuilder().content(this.record.LOC[i]).build());
-                this.addLine(new ViewFieldBuilder().label("Location code").build(), fieldsArray);
+                this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.LocationCode").build(), fieldsArray);
             }
         }
 
         fieldsArray = new Array<ViewField>();
         fieldsArray.push(new ViewFieldBuilder().content(this.record.GRPCODE).build());
-        this.addLine(new ViewFieldBuilder().label("Offset code").build(), fieldsArray);
+        this.addLine(new ViewFieldBuilder().label("ILL.MemberInfo.OffsetCode").build(), fieldsArray);
 
         return this.viewLines;
     }
