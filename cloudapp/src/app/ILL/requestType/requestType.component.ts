923a134650ba0f56d395d3b31abaed32bc72f592
jawanbdarni <jawan.bdarni@clarivate.com>
2024-11-20 11:16:35 +0200

    URM-234843
    ILL borrowing - Add selection of Copy/Loan page at the beginning of the process

--- cloudapp/src/app/ILL/requestType/requestType.component.ts
+++ cloudapp/src/app/ILL/requestType/requestType.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SELECTED_REQUEST_TYPE } from '../../service/base.service';
-import {  CloudAppStoreService } from '@exlibris/exl-cloudapp-angular-lib';
+import { CloudAppStoreService } from '@exlibris/exl-cloudapp-angular-lib';
 
 @Component({
   selector: 'ILL-requestType',
@@ -18,10 +18,9 @@ export class RequestTypeComponent implements OnInit {
     private fb: FormBuilder,
     private router: Router,
     private storeService: CloudAppStoreService,
-
   ) {
     this.form = this.fb.group({
-      requestType: ['']  
+      requestType: ['COPYO']  
     });
   }
 
