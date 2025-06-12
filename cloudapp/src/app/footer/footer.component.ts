de3d595518ecec6982d1ac8c8e7940e2f837e1f6
Dorit Cohen <dorit.cohen@clarivate.com>
2025-06-12 17:05:07 +0300

    URM-250162
    Add use of the general storeService

--- cloudapp/src/app/footer/footer.component.ts
+++ cloudapp/src/app/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { AlertService, CloudAppStoreService } from '@exlibris/exl-cloudapp-angular-lib';
 
 @Component({
   selector: 'app-footer',
@@ -9,22 +10,25 @@ import { TranslateService } from '@ngx-translate/core';
 export class FooterComponent implements OnInit {
 
   constructor(
-    private translate: TranslateService
+    private translate: TranslateService,
+    private storeService: CloudAppStoreService
   ) { }
 
   ngOnInit() {
-    const savedLang = localStorage.getItem('currentLang') || 'jp';
-    this.translate.use(savedLang);  
-  
-    if (!localStorage.getItem('currentLang')) {
-      localStorage.setItem('currentLang', 'jp');  
-    }
+      this.storeService.get('currentLang').subscribe(lang=>{
+        if (!lang) {
+          this.translate.use('jp');
+        } else {
+          this.translate.use(lang);
+        }
+      });
   }
   
 
   setLang(lang: string) {
     this.translate.use(lang);
-    localStorage.setItem('currentLang', lang);
+    this.storeService.set('currentLang',lang).subscribe();   
+
   }
 
 }
