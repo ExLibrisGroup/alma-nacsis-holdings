773f63db8fa725c8da24f8b11fd90986ea75b907
Dorit Cohen <dorit.cohen@clarivate.com>
2025-07-17 14:46:21 +0300

    URM-236927
    Display Issue: Unnecessary double vertical bars appear in UTL

--- cloudapp/src/app/catalog/results-types/monographs.ts
+++ cloudapp/src/app/catalog/results-types/monographs.ts
@@ -401,7 +401,9 @@ export class MonographFullDisplay extends IDisplayLines {
                 fieldsArray.push(new ViewFieldBuilder().content(utl.UTHDNG).build());
                 fieldsArray.push(new ViewFieldBuilder().content(utl.UTHDNGR).build());
                 fieldsArray.push(new ViewFieldBuilder().content(utl.UTHDNGVR).build());
-                fieldsArray = this.setSeparator(fieldsArray, "||");
+                if(utl.UTFLG !== "*" && (!this.isEmpty(utl.UTHDNGR) || !this.isEmpty(utl.UTHDNGVR))){
+                    fieldsArray = this.setSeparator(fieldsArray, "||");
+                }
                 fieldsArray.push(new ViewFieldBuilder().content(utl.UTID).link(SearchType.Names).build());
                 fieldsArray.push(new ViewFieldBuilder().content(utl.UTINFO).build());
             this.addLine(new ViewFieldBuilder().label("UTL").build(), fieldsArray);
